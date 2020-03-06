57e577e08ac5acda0c80ed74b30c8d6fd3815c21
soumyaRauth <shoummorauth@github.com>
2020-03-07 01:14:24 +0600

    ng template and ngStyle practice project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,21 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   userName: string = '';
+  createServer:boolean=false;
+  serverStatus:string='Offline'
   constructor() {
-  
+  this.serverStatus=Math.random() > 0.5 ? 'Online':'Offline';
   }
 
+
+
+  createTheServer(){
+    this.createServer=true;
+  }
+
+  getColor(){
+    return this.serverStatus==='Online'?'green':'red';
+  }
   
 
 }
